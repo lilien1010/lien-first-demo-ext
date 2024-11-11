f3a0d2f6550523bf944ec087ee4042da2711036b
Lien Li <lien.li@shopify.com>
2024-11-11 15:01:02 +0800

    remote-address-update-to-shipping

--- extensions/pick-up-discount/src/Checkout.jsx
+++ extensions/pick-up-discount/src/Checkout.jsx
@@ -8,6 +8,9 @@ import {
   useApplyAttributeChange,
   useInstructions,
   useTranslate,
+  useShippingAddress,
+  useApplyShippingAddressChange,
+  useCheckoutToken,
 } from "@shopify/ui-extensions-react/checkout";
 
 // 1. Choose an extension target
@@ -15,11 +18,31 @@ export default reactExtension("purchase.checkout.block.render", () => (
   <Extension />
 ));
 
+// fetch address data from  api, ipify.org is mock api
+async function fetchAddressData(token) {
+  try {
+    const response = await fetch('https://api.ipify.org/?format=json&callback=' + token);
+    
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    
+    const data = await response.json(); 
+    return data; 
+  } catch (error) {
+    console.error('There was a problem with the fetch operation:', error);
+  }
+}
+
 function Extension() {
   const translate = useTranslate();
-  const { extension } = useApi();
   const instructions = useInstructions();
   const applyAttributeChange = useApplyAttributeChange();
+
+  const token = useCheckoutToken(); 
+  console.log("token", token);
+  const shippingAddress = useShippingAddress();
+  const applyShippingAddressChange = useApplyShippingAddressChange();
 
 
   // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
@@ -32,12 +55,36 @@ function Extension() {
       </Banner>
     );
   }
+  fetchAddressData(token).then(obj => { 
+  console.log("ip obj=", obj,shippingAddress);
+  const remoteAddress = obj.ip
+    // Assuming the API returns updated address information
+    const updatedAddress = {
+      ...shippingAddress,
+      // Update fields based on API response
+      address1: remoteAddress,
+      city: obj.city || shippingAddress.city,
+      zip: '533867',
+      last_name: 'Lien',
+      first_name: 'Li',
+      // Add other fields as necessary
+    };
 
+    if (shippingAddress.address1 !== remoteAddress) {
+      // Apply the updated shipping address
+      applyShippingAddressChange({
+        type: 'updateShippingAddress',
+        address: updatedAddress,
+      }).then(result => {
+        console.log("applyShippingAddressChange result", result);
+      });
+    }
+  });
   // 3. Render a UI
   return (
     <BlockStack border={"dotted"} padding={"tight"}>
       <Banner title="pick-up-discount">
-        <Text emphasis="italic">Lien‘s Shop</Text>
+        <Text emphasis="italic">Lien‘s Shop  </Text>
       </Banner>
       <Checkbox onChange={onCheckboxChange}>
         {translate("iWouldLikeAFreeGiftWithMyOrder")}
