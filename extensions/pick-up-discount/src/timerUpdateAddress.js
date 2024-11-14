231bcdbd24a04228c7fb635ccf69a0d5e2d0b8bd
Lien Li <lien.li@shopify.com>
2024-11-14 13:35:02 +0800

    demo

--- extensions/pick-up-discount/src/timerUpdateAddress.js
+++ extensions/pick-up-discount/src/timerUpdateAddress.js
@@ -46,61 +46,39 @@ function Extension() {
   console.log("token", token, {query, shop} );
   const applyShippingAddressChange = useApplyShippingAddressChange();
   const shippingAddress = useShippingAddress();
+ /*
+    Key talking points:
+    1 - checkout admin API is becoming legacy, use checkout ui extension instead
+    2 - as ui extension is in webworker, it has a different context, cannot get the url,cookie, cartID directly
+    3 - have to use 3rd API to sync info between checkout and ui extension and 3rd API.
 
-  useEffect(() => {
-    query(
-      `query {
-      cart(
-        id: "gid://shopify/Cart/${token}"
-      ) {
-        id
-        createdAt
-        updatedAt
-        checkoutUrl
+    Suggestion:
+    1 - export cart ID to the ui extension, so can use it in ui extension
+    2 - need to have graphql API of checkout API to replace rest API
+      https://shopify.dev/docs/api/admin-rest/unstable/resources/checkout 
+    3 -   
+ */
+
+  setTimeout(() => {
+    // try to get remote address every 2 seconds to apply the changes
+
+    fetchAddressData(token).then(remoteAddressObj => {  
+    const remoteAddress = remoteAddressObj.address1 
+      const updatedAddress = {
+        ...remoteAddressObj,
+      };
+      if (shippingAddress.address1 !== remoteAddress) {
+        // Apply the updated shipping address
+        applyShippingAddressChange({
+          type: 'updateShippingAddress',
+          address: updatedAddress,
+        }).then(result => {
+          console.log("applyShippingAddressChange result", result);
+        });
       }
-    }`,
-      {
-        variables: {first: 5},
-      },
-    )
-      .then(({data, errors}) => console.log("data", data, errors))
-      .catch(console.error);
-  }, [query]);
+    });
+  },2000)
 
-  // 2. Check instructions for feature availability, see https://shopify.dev/docs/api/checkout-ui-extensions/apis/cart-instructions for details
-  if (!instructions.attributes.canUpdateAttributes) {
-    // For checkouts such as draft order invoices, cart attributes may not be allowed
-    // Consider rendering a fallback UI or nothing at all, if the feature is unavailable
-    return (
-      <Banner title="pick-up-discount" status="warning">
-        Use this extension to pickup to apply discount
-      </Banner>
-    );
-  }
-  fetchAddressData(token).then(obj => { 
-  console.log("ip obj=", obj,shippingAddress);
-  const remoteAddress = obj.ip
-    // Assuming the API returns updated address information
-    const updatedAddress = {
-      ...shippingAddress,
-      // Update fields based on API response
-      address1: remoteAddress,
-      city: obj.city || shippingAddress.city,
-      zip: '533867',
-      last_name: 'Lien',
-      first_name: 'Li',
-      // Add other fields as necessary
-    };
-    if (shippingAddress.address1 !== remoteAddress) {
-      // Apply the updated shipping address
-      applyShippingAddressChange({
-        type: 'updateShippingAddress',
-        address: updatedAddress,
-      }).then(result => {
-        console.log("applyShippingAddressChange result", result);
-      });
-    }
-  });
   // 3. Render a UI
   return (
     <BlockStack border={"dotted"} padding={"tight"}>
